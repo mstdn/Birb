0b2842ba18cfa1b7782b2259075a73d3abf679ac
stux <hello@mstdn.social>
2022-07-08 20:55:47 +0200

    Sm0ll UI tweaks and such mew

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,21 +1,24 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <meta property="og:site_name" content="{{ config('app.name', 'Birb') }}">
+    <meta property="og:title" content="{{ config('app.name', 'Birb') }} Social Network">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="{{request()->url()}}">
+    <!-- Favicon -->
+    <link href="{{ asset('favicon.png') }}" rel="icon" type="image/png">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">`
 </head>
